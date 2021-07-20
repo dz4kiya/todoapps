be35e492f5d116f713ede80e84dbe3c1d6e3ddc2
dz4kiya <dishmatul@gmail.com>
2021-07-20 22:17:52 +0700

    finished

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,7 @@
 const UNCOMPLETED_LIST_TODO_ID = "todos";
+const COMPLETED_LIST_TODO_ID = "completed-todos";
 
-function makeTodo(data, timestamp) {
+function makeTodo(data, timestamp, isCompleted) {
     const textTitle = document.createElement("h2");
     textTitle.innerText = data;
 
@@ -12,8 +13,16 @@ function makeTodo(data, timestamp) {
     textContainer.append(textTitle, textTimestamp); //memasukkan textTitle dan textTimestamp ke textContainer
 
     const container = document.createElement("div");
-    container.classList.add("item","shadow")
+    container.classList.add("item", "shadow")
     container.append(textContainer);
+
+    if (isCompleted) {
+        container.append(
+            createUndoButton(),
+            createTrashBUtton());
+    } else {
+        container.append(createCheckButton());
+    }
 
     return container;
 }
@@ -35,6 +44,59 @@ function addTodo() {
     const todo = makeTodo(textTodo, timestamp);
     uncompletedTODOList.append(todo);
 
-    
 }
 
+function createButton(buttonTypeClass, eventListener) {
+    const button = document.createElement("button");
+    button.classList.add(buttonTypeClass);
+    button.addEventListener("click", function (event) {
+        eventListener(event);
+    });
+    return button;
+
+}
+
+function addTaskToCompleted(taskElement) {
+    const taskTitle = taskElement.querySelector(".inner > h2").innerText;
+    const taskTimestamp = taskElement.querySelector(".inner > p").innerText;
+
+    const newTodo = makeTodo(taskTitle, taskTimestamp, true);
+    const listCompleted = document.getElementById(COMPLETED_LIST_TODO_ID);
+    listCompleted.append(newTodo);
+
+    taskElement.remove();       //menghapus todo yang belum selesai
+}
+
+function createCheckButton() {
+    return createButton("check-button", function (event) {
+        addTaskToCompleted(event.target.parentElement);
+    });
+}
+
+function removeTaskFromCompleted(taskElement) {
+    taskElement.remove();
+}
+
+function createTrashBUtton() {
+    return createButton("trash-button", function (event) {
+        removeTaskFromCompleted(event.target.parentElement);
+    });
+}
+
+function undoTaskFromCompleted(taskElement) {
+    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
+    const taskTitle = taskElement.querySelector(".inner > h2").innerText;
+    const taskTimestamp = taskElement.querySelector(".inner > p").innerText;
+
+    const newTodo = makeTodo(taskTitle, taskTimestamp, false);
+
+    listUncompleted.append(newTodo);
+    taskElement.remove();
+
+}
+
+function createUndoButton() {
+    return createButton("undo-button", function (event) {
+        undoTaskFromCompleted(event.target.parentElement);
+    });
+}
